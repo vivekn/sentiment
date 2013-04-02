2ccada626abea78616136b8680a2f56ba817a1fc
Vivek Narayanan <vivek_n@me.com>
2013-04-03 00:02:54 +0530

    Trigrams implemented efficiently

--- info.py
+++ info.py
@@ -1,15 +1,27 @@
 from __future__ import division
 from math import log
-from collections import defaultdict
 from operator import mul
+from collections import Counter
 import os
 import pylab
+import cPickle
 
-pos = defaultdict(int)
-neg = defaultdict(int)
+
+class MyDict(dict):
+    def __getitem__(self, key):
+        if key in self:
+            return self.get(key)
+        return 0
+
+pos = MyDict()
+neg = MyDict()
 features = set()
 totals = [0, 0]
 delchars = ''.join(c for c in map(chr, range(128)) if not c.isalnum())
+
+CDATA_FILE = "countdata.pickle"
+FDATA_FILE = "reduceddata.pickle"
+
 
 def negate_sequence(text):
     """
@@ -20,6 +32,7 @@ def negate_sequence(text):
     result = []
     words = text.split()
     prev = None
+    pprev = None
     for word in words:
         # stripped = word.strip(delchars)
         stripped = word.strip(delims).lower()
@@ -28,6 +41,10 @@ def negate_sequence(text):
         if prev:
             bigram = prev + " " + negated
             result.append(bigram)
+            if pprev:
+                trigram = pprev + " " + bigram
+                result.append(trigram)
+            pprev = prev
         prev = negated
 
         if any(neg in word for neg in ["not", "n't", "no"]):
@@ -38,8 +55,16 @@ def negate_sequence(text):
 
     return result
 
+
 def train():
     global pos, neg, totals
+    retrain = False 
+    
+    # Load counts if they already exists
+    if not retrain and os.path.isfile(CDATA_FILE):
+        pos, neg, totals = cPickle.load(open(CDATA_FILE))
+        return
+
     limit = 12500
     for file in os.listdir("./aclImdb/train/pos")[:limit]:
         for word in set(negate_sequence(open("./aclImdb/train/pos/" + file).read())):
@@ -49,8 +74,14 @@ def train():
         for word in set(negate_sequence(open("./aclImdb/train/neg/" + file).read())):
             neg[word] += 1
             pos['not_' + word] += 1
+    
+    prune_features()
+
     totals[0] = sum(pos.values())
     totals[1] = sum(neg.values())
+    
+    countdata = (pos, neg, totals)
+    cPickle.dump(countdata, open(CDATA_FILE, 'w'))
 
 def classify(text):
     words = set(word for word in negate_sequence(text) if word in features)
@@ -78,22 +109,40 @@ def MI(word):
         I += pos[word] / T * log (pos[word] * T / W / totals[0])
     return I
 
-def feature_selection_experiment():
+def get_relevant_features():
+    pos_dump = {k: pos[k] for k in pos if k in features}
+    neg_dump = {k: pos[k] for k in neg if k in features}
+    totals_dump = [sum(pos_dump.values()), sum(neg_dump.values())]
+    return (pos_dump, neg_dump, totals_dump)
+
+def prune_features():
+    global pos, neg
+    for k in pos.keys():
+        if pos[k] <= 1 and neg[k] <= 1:
+            del pos[k]
+
+    for k in neg.keys():
+        if neg[k] <= 1 and pos[k] <= 1:
+            del neg[k]
+
+def feature_selection_trials():
     """
-    Select top k features. Vary k from 1000 to 50000 and plot data
+    Select top k features. Vary k and plot data
     """
     global pos, neg, features
     words = list(set(pos.keys() + neg.keys()))
     print "Total no of features:", len(words)
     words.sort(key=lambda w: -MI(w))
     num_features, accuracy = [], []
-    limit = 12500
+    bestk = 0
+    limit = 500
     path = "./aclImdb/test/"
-    step = 1000
-    start = 14000
+    step = 500
+    start = 31000
+    best_accuracy = 0.0
     for w in words[:start]:
         features.add(w)
-    for k in xrange(start, 30000, step):
+    for k in xrange(start, 33000, step):
         for w in words[k:k+step]:
             features.add(w)
         correct = 0
@@ -109,11 +158,16 @@ def feature_selection_experiment():
 
         num_features.append(k+step)
         accuracy.append(correct / size)
+        if (correct / size) > best_accuracy:
+            bestk = k
         print k+step, correct / size
+
+    features = set(words[:bestk])
+    # cPickle.dump(get_relevant_features(), open(FDATA_FILE, 'w'))
 
     pylab.plot(num_features, accuracy)
     pylab.show()
 
 if __name__ == '__main__':
     train()
-    feature_selection_experiment()
+    feature_selection_trials()
